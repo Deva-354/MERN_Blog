bdafa311d1fd1e5e9b35de7dfc14439b9e88c7a2
Deva-354 <130522847+Deva-354@users.noreply.github.com>
2024-02-24 02:20:55 -0500

    Add files via upload

---
+++ src/components/home/post/Post.jsx
@@ -0,0 +1,57 @@
+import { styled, Box, Typography } from '@mui/material';
+import {addEllipsis} from '../../../utils/common-utils';
+
+const Container = styled(Box)`
+    border: 1px solid #d3cede;
+    border-radius: 10px;
+    margin: 10px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    height: 350px;
+    & > p {
+        padding: 0 5px 5px 5px;
+    }
+`;
+
+const Image = styled('img')({
+    width: '100%',
+    objectFit: 'cover',
+    borderRadius: '10px 10px 0 0',
+    height: 150
+});
+
+const Text = styled(Typography)`
+    color: #878787
+    font-size: 12px;
+`;
+
+const Heading = styled(Typography)`
+    font-size: 18px;
+    font-weight: 600
+`;
+
+const Details = styled(Typography)`
+    font-size: 14px;
+    word-break: break-word;
+`;
+
+const Post = ({ post }) => {
+    const url = post.picture ? post.picture : 'https://nsiteam.com/social/wp-content/uploads/2014/11/blog-background-1.jpg';
+    
+    const addEllipsis = (str, limit) => {
+        return str.length > limit ? str.substring(0, limit) + '...' : str;
+    } 
+
+    return (
+        <Container>
+            <Image src={post.picture} alt="blog" />
+            <Text>{post.categories}</Text>
+            <Heading>{addEllipsis(post.title, 20)}</Heading>
+            <Text>Author: {post.username}</Text>
+            <Details>{addEllipsis(post.description, 100)}</Details>
+        </Container>
+    )
+}
+
+export default Post;
