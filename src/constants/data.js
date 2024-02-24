aa762cddbaebf839e607e89dac5c58bf34213dcd
Deva-354 <130522847+Deva-354@users.noreply.github.com>
2024-02-24 02:22:23 -0500

    Delete src directory

--- src/constants/data.js
+++
@@ -1,7 +0,0 @@
-export const categories = [
-    { id: 1, type: "Travel" },
-    { id: 2, type: "Lifestyle" },
-    { id: 3, type: "Health&Fitness" },
-    { id: 4, type: "Tech" },
-    { id: 5, type: "Fashion" }
-];
